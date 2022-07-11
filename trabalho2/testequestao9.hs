5b1882bb67f790dc1307dea82c857b9148c4bab6
Nathália Pereira <np.nathaliapereira@gmail.com>
2022-07-10 23:39:58 -0300

    Add tests question 9

---
+++ trabalho2/testequestao9.hs
@@ -0,0 +1,54 @@
+med1 :: Medicamento
+med1 = "Adera"
+
+med2 :: Medicamento
+med2 = "Alprazolam"
+
+med3 :: Medicamento
+med3 = "Donepezila"
+
+med4 :: Medicamento
+med4 = "Lactulona"
+
+med5 :: Medicamento
+med5 = "Mirtazapina"
+
+med6 :: Medicamento
+med6 = "Pantoprazol"
+
+med7 :: Medicamento
+med7 = "Patz"
+
+med8 :: Medicamento
+med8 = "Quetiapina"
+
+med9 :: Medicamento
+med9 = "Xarelto"
+
+type Medicamento = String
+
+type Quantidade = Int
+
+type Horario = Int
+
+type EstoqueMedicamentos = [(Medicamento, Quantidade)]
+
+type Plantao = [(Horario, [Cuidado])]
+
+data Cuidado = Comprar Medicamento Quantidade | Medicar Medicamento
+
+instance Show Cuidado where
+  show (Comprar m q) =
+    "Comprar "
+      ++ Prelude.show q
+      ++ " comprimido(s) do medicamento: "
+      ++ m
+  show (Medicar m) = "Ministrar medicamento: " ++ m
+
+
+executaPlantao :: Plantao -> EstoqueMedicamentos -> Maybe EstoqueMedicamentos
+executaPlantao = undefined
+
+main :: IO ()
+main =  do
+print("oi")
